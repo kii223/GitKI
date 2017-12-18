c484a492e49913aff1dac35221ba5cb90adf27a1
kii223 <34079551+kii223@users.noreply.github.com>
2017-12-18 11:24:40 +0800

    导入相关库

--- OpenGL_Study/view.py
+++ OpenGL_Study/view.py
@@ -1,1 +1,13 @@
 import numpy
+from numpy.linalg import norm, inv
+from OpenGL.GL import glCallList, glClear, glClearColor, glColorMaterial, glCullFace, glDepthFunc, glDisable,\
+                      glFlush, glGetFloatv, glLightfv, glLoadIdentity, glMatrixMode, glMultMatrixf, glPopMatrix, \
+                      glPushMatrix, glTranslated, glViewport, glEnable,\
+                      GL_AMBIENT_AND_DIFFUSE, GL_BACK, GL_CULL_FACE, GL_COLOR_BUFFER_BIT, GL_COLOR_MATERIAL, \
+                      GL_DEPTH_BUFFER_BIT, GL_DEPTH_TEST, GL_FRONT_AND_BACK, GL_LESS, GL_LIGHT0, GL_LIGHTING, \
+                      GL_MODELVIEW, GL_MODELVIEW_MATRIX, GL_POSITION, GL_PROJECTION, GL_SPOT_DIRECTION
+
+
+
+
+
