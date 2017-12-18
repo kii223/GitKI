a728a38e434a48bb4f9a371efd94df7f6a75d022
kii223 <34079551+kii223@users.noreply.github.com>
2017-12-18 11:48:17 +0800

    导入相关库，更改文件名

--- OpenGL_Study/viewer.py
+++ OpenGL_Study/viewer.py
@@ -6,6 +6,11 @@ from OpenGL.GL import glCallList, glClear, glClearColor, glColorMaterial, glCull
                       GL_AMBIENT_AND_DIFFUSE, GL_BACK, GL_CULL_FACE, GL_COLOR_BUFFER_BIT, GL_COLOR_MATERIAL, \
                       GL_DEPTH_BUFFER_BIT, GL_DEPTH_TEST, GL_FRONT_AND_BACK, GL_LESS, GL_LIGHT0, GL_LIGHTING, \
                       GL_MODELVIEW, GL_MODELVIEW_MATRIX, GL_POSITION, GL_PROJECTION, GL_SPOT_DIRECTION
+from OpenGL.constants import GLfloat_3, GLfloat_4
+from OpenGL.GLU import gluPerspective, gluUnProject
+from OpenGL.GLUT import glutCreateWindow, glutDisplayFunc, glutGet, glutInit, glutInitDisplayMode, \
+                        glutInitWindowSize, glutMainLoop, \
+                        GLUT_SINGLE, GLUT_RGB, GLUT_WINDOW_HEIGHT, GLUT_WINDOW_WIDTH
 
 
 
