84b58ada54344973607d7094b47977b02de8974b
kii223 <34079551+kii223@users.noreply.github.com>
2017-12-18 17:30:57 +0800

    导入相关库，更改文件名

--- OpenGL_Study/viewer.py
+++ OpenGL_Study/viewer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy
 from numpy.linalg import norm, inv
 from OpenGL.GL import glCallList, glClear, glClearColor, glColorMaterial, glCullFace, glDepthFunc, glDisable,\
@@ -11,6 +12,31 @@ from OpenGL.GLU import gluPerspective, gluUnProject
 from OpenGL.GLUT import glutCreateWindow, glutDisplayFunc, glutGet, glutInit, glutInitDisplayMode, \
                         glutInitWindowSize, glutMainLoop, \
                         GLUT_SINGLE, GLUT_RGB, GLUT_WINDOW_HEIGHT, GLUT_WINDOW_WIDTH
+
+
+class Viewer(object):
+    def __init__(self):
+        self.init_interface()       # 初始化接口，创建窗口并注册渲染函数
+        self.init_opengl()          # 初始化opengl配置
+        self.init_scene()           # 初始化3D场景
+        self.init_interaction()     # 初始化交互操作代码
+
+    def init_interface(self):
+        '''初始化窗口并注册渲染函数'''
+        glutInit()
+        glutInitWindowSize(640, 480)
+        glutCreateWindow('3D Modeller')
+        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+        '''注册窗口渲染函数'''
+        glutDisplayFunc(self.render)
+
+    def init_opengl(self):
+        """ 初始化opengl的配置 """
+        # 模型视图矩阵
+        self.inverseModelView = numpy.identity(4)
+        # 模型视图矩阵的逆矩阵
+        self.modelView = numpy.identity(4)
+
 
 
 
