91bdb076abb9802ee3203c3ae880f8bcf52f1b31
kii223 <34079551+kii223@users.noreply.github.com>
2017-12-22 13:28:09 +0800

    导入相关库，更改文件名

---
+++ Chanllage.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+# 字符表替换 1关
+# intab = 'abcdefghijklmnopqrstuvwxyz'
+# outab = 'cdefghijklmnopqrstuvwxyzab'
+#
+# transtab = str.maketrans(intab,outab)
+#
+# text = 'map'
+#
+# out = text.translate(transtab)
+#
+# print(out)
+
+
+# 字符查找 2关
+# import requests # 网络请求
+# import urllib.request as urlopen
+# import re
+# url = 'http://www.pythonchallenge.com/pc/def/ocr.html'
+# wb_data = urlopen.urlopen(url)
+# res = str(wb_data.read())
+# print(res)
+# mes = re.findall(r'<!--.*-->', res)
+#
+# mes1 = mes[0].replace('find rare characters in the mess below:', '')
+# mes2 = mes1.replace(r'\n', '')
+# pasw = re.findall(r'[a-z]|[A-Z]|[0-9]', mes2)
+# url = ''
+# for i in pasw:
+#     url += i
+#
+# print(url)
+
+
+# 字符查找 3关
+# import requests # 网络请求
+# import urllib.request as urlopen
+# import re
+# url = 'http://www.pythonchallenge.com/pc/def/equality.html'
+# wb_data = urlopen.urlopen(url)
+# res = str(wb_data.read())
+#
+# mes = re.findall(r'<!--.*-->', res)
+# mes1 = mes[0].replace(r'\n', '')
+# pasw = re.findall(r'[a-z][A-Z]{3}[a-z][A-Z]{3}[a-z]', mes1)
+#
+# pasw1 = ''.join(pasw)
+#
+#
+# pasw2 = re.findall(r'[A-Z]{3}[a-z][A-Z]{3}', pasw1)
+# pasw3 = ''.join(pasw2)
+#
+# pasw4 = re.findall(r'[a-z]', pasw3)
+# pasw5 = ''.join(pasw4)
+# print(pasw5)
+
+# 网址链 4关
+# import requests  # 网络请求
+# import urllib.request as urlopen
+# import re
+# import time
+#
+#
+# n = 0
+# index = '63579'
+#
+# while n < 400:
+#     url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s'% index
+#     wb_data = urlopen.urlopen(url)
+#     res = str(wb_data.read())
+#     nexturl = re.findall(r'[0-9]', res)
+#     index = str(int(int(''.join(nexturl))))
+#     print(res + 'next index is:' + index)
+#     n += 1
+#     print('%d--------------' % n)
+#     time.sleep(0.7)
+
+
+# peak hell 5关
+import requests  # 网络请求
+import urllib.request as urlopen
+import re
+import time
+import pickle
+
+
+
+# url = 'http://www.pythonchallenge.com/pc/def/banner.p'
+# wb_data = urlopen.urlopen(url)
+# res = str(wb_data.read())
+# print(res)
+# f = open('D:/python/GitKI/123.txt', 'rb')
+# d = pickle.load(f)
+# f.close()
+# print(d)
+
+with open('f.txt', 'r') as f:
+    a = f.read()
+    obj = pickle.loads(a.encode('utf-8'))
+    print(type(obj))
+
+
